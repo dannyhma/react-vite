184eb423da0fbd58a81290b728d8e2c18f715160
dannyhma <dannyhimawan089@gmail.com>
2023-12-08 23:09:11 +0700

    react components

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,36 @@
+/* eslint-disable react/prop-types */
+import { IconBrandTwitter } from "@tabler/icons-react";
+
 export default function App() {
 	return (
 		<>
 			<section className="container mx-auto">
-				<div className="flex h-32">
-					<h1 className="text-Purple m-10 flex-1 text-center text-2xl">Learn react</h1>
-					<div className=" m-10 mx-auto flex-1 text-center">
-						<button className="bg-Purple shadow-smshadow grow rounded-full px-8 py-4 text-sm font-bold text-white ">
-							Login
-						</button>
+				<div className="grid h-32 grid-cols-12">
+					<h1 className="col-span-9 m-10 text-2xl text-Purple">Learn react</h1>
+					<div className="col-span-3 m-10">
+						<div className="flex justify-center gap-x-2">
+							<Button>
+								<IconBrandTwitter />
+								Login
+							</Button>
+							<Button text="Sign up" />
+						</div>
 					</div>
 				</div>
 			</section>
 		</>
-	)
+	);
+}
+
+export function Button(props) {
+	const { children, text, type } = props;
+
+	return (
+		<button
+			type={type}
+			className="flex gap-x-2 rounded-full bg-Purple px-10 py-4 text-sm font-bold text-white shadow-smshadow"
+		>
+			{text || children}
+		</button>
+	);
 }
