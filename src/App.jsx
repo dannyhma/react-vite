d29fd486a54e05c2e0e97d629d7593b38e536168
dannyhma <dannyhimawan089@gmail.com>
2023-12-09 14:22:21 +0700

    react-components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
-import { IconBrandTwitter } from "@tabler/icons-react";
+import { IconBrandFacebook, IconBrandTwitter } from "@tabler/icons-react";
+import clsx from "clsx";
 
 export default function App() {
 	return (
@@ -9,11 +10,27 @@ export default function App() {
 					<h1 className="col-span-9 m-10 text-2xl text-Purple">Learn react</h1>
 					<div className="col-span-3 m-10">
 						<div className="flex justify-center gap-x-2">
-							<Button>
+							<Button
+								{...{
+									type: "button",
+									onClick: function () {
+										console.log("Register");
+									}
+								}}
+								className="bg-Twitter"
+							>
 								<IconBrandTwitter />
+								Register
+							</Button>
+							<Button
+								onClick={function () {
+									console.log("Login");
+								}}
+								className="bg-Facebook"
+							>
+								<IconBrandFacebook />
 								Login
 							</Button>
-							<Button text="Sign up" />
 						</div>
 					</div>
 				</div>
@@ -23,12 +40,16 @@ export default function App() {
 }
 
 export function Button(props) {
-	const { children, text, type } = props;
+	const { className = "bg-Tw", children, text, type = "submit" } = props;
 
 	return (
 		<button
+			{...props}
 			type={type}
-			className="flex gap-x-2 rounded-full bg-Purple px-10 py-4 text-sm font-bold text-white shadow-smshadow"
+			className={clsx(
+				className,
+				"${className} flex gap-x-2 rounded-full px-10 py-4 text-sm font-bold text-white shadow-smshadow [&>svg]:h-5 [&>svg]:w-5 [&>svg]:stroke-1"
+			)}
 		>
 			{text || children}
 		</button>
